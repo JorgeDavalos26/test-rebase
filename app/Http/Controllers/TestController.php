7de552fffe2edc1604900d7d6374d9dc8f232bda
Jorge <jorge6alberto@gmail.com>
2023-01-09 15:45:00 -0600

    A2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
@@ -23,7 +24,7 @@ class TestController extends Controller
      */
     public function create()
     {
-        //
+        User::where('email', 'test')->get();
     }
 
     /**
